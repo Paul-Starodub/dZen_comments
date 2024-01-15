ea47085d50e7e2647b6fe3477b147c403335e774
Paul Starodub <amorallex@gmail.com>
2024-01-15 23:34:25 +0200

    update comments/urls.py with need endpoints

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
+from comments.views import add_comment, comment_detail, comment_list
 
-urlpatterns = []
+
+urlpatterns = [
+    path("add/", add_comment, name="add_comment"),
+    path("", comment_list, name="comment_list"),
+    path("<int:comment_id>/", comment_detail, name="comment_detail"),
+]
 
 app_name = "comments"
