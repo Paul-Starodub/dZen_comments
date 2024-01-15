e7c4ecb403e59631424096a2728617d984f4078a
Paul Starodub <amorallex@gmail.com>
2024-01-15 23:47:06 +0200

    update requirements.txt & forms.py & settings.py & urls.py for captcha

--- dZEN_comments/urls.py
+++ dZEN_comments/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("captcha/", include("captcha.urls")),
     path("", include("comments.urls"), name="comments"),
-
 ]
